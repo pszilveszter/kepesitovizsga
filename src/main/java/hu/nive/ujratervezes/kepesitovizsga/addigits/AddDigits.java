deeacf85faba679111a27b3edde94b58a1c9f209
Szilveszter Pinter <szilveszter.pinter@betssongroup.com>
2021-04-29 10:44:59 +0200

    addigits

---
+++ src/main/java/hu/nive/ujratervezes/kepesitovizsga/addigits/AddDigits.java
@@ -0,0 +1,23 @@
+package hu.nive.ujratervezes.kepesitovizsga.addigits;
+
+public class AddDigits {
+
+    public int addDigits(String s) {
+        if (isEmptyIn(s)) {
+            return -1;
+        }
+
+        int sum = 0;
+        for (Character c : s.toCharArray()) {
+            if (Character.isDigit(c)) {
+                sum += Integer.parseInt(c.toString());
+            }
+        }
+        return sum;
+    }
+
+
+    private boolean isEmptyIn(String s) {
+        return s == null || s.isBlank();
+    }
+}
