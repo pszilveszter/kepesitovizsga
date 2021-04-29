d6f5902743174848c5fb04acf67d0cc32222f541
Szilveszter Pinter <szilveszter.pinter@betssongroup.com>
2021-04-29 10:46:53 +0200

    phonebook

---
+++ src/main/java/hu/nive/ujratervezes/kepesitovizsga/phonebook/Phonebook.java
@@ -0,0 +1,26 @@
+package hu.nive.ujratervezes.kepesitovizsga.phonebook;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+
+
+public class Phonebook {
+    public void exportPhonebook(Map<String, String> contacts, String output) {
+        if (contacts == null || output == null) {
+            throw new IllegalArgumentException("Missing parameter, unable to continnue");
+        }
+
+        Path file = Path.of(output);
+        try (BufferedWriter writer = Files.newBufferedWriter(file)) {
+            for (Map.Entry<String, String> entry: contacts.entrySet()) {
+                writer.write(entry.getKey() + ": " + entry.getValue() + "\n");
+            }
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Unable to write given file");
+        }
+
+    }
+}
